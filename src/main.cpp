d6b78c10a9bc41ac6e50fdc1b04cbc517600d8ad
Krisjanis Ivbulis <ivbulisk@gmail.com>
2023-04-01 17:39:53 +0300

    moved window and renderer object creation to main

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,26 @@
 #include <SDL2/SDL_image.h>
 
 #include "game.h"
+#include "templates.cpp"
+
 
 
 int main(int argc, char *argv[])
 {
+    const int disp_size_x = 1000;
+    const int disp_size_y = 640;
 
-    game game(640, 640);
+    int game_state[8][8];
+    
+    init_game_state<true>(game_state);
 
-    game.init();
+    //TODO: catch exceptions 
+    SDL_Window *window = SDL_CreateWindow("Checkers", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, disp_size_x, disp_size_y, 0);
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    game game(window, renderer);
     game.draw_board(true);
     game.events();
-
 
     return 0;
 }
