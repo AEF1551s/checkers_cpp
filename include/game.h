5f447e2e4fe4718c79eff444733442f29265765f
Krisjanis Ivbulis <ivbulisk@gmail.com>
2023-04-02 14:51:22 +0300

    added init_game_state method

--- include/game.h
+++ include/game.h
@@ -1,20 +1,23 @@
 #if !defined(GAME_H_INCLUDED)
 #define GAME_H_INCLUDED
 #include <SDL2/SDL.h>
+#include "checkers.h"
 
 class game
 {
 private:
     SDL_Window *window;
     SDL_Renderer *renderer;
-    int size_x, size_y;
     int pos_xy[2], rect_xy[2];
+    static int game_state[8][8];
 
 public:
-    game(int size_x, int size_y);
-    void init();
+    game(SDL_Window *window, SDL_Renderer *renderer);
+    void init_game_state(bool first_player);
     void draw_board(bool first_player);
-    void events();
+    void render_game_state(); //TODO: Render all pieces on board based on game_state
+    void events(SDL_bool &done);
+    void game_loop();
 };
 
 #endif // GAME_H_INCLUDED
--- src/game.cpp
+++ src/game.cpp
@@ -1,29 +1,33 @@
 #include "game.h"
+#include "templates.cpp"
 #include <iostream>
 
-game::game(int size_x, int size_y)
+int game::game_state[8][8];
+
+game::game(SDL_Window *window, SDL_Renderer *renderer)
 {
-    this->size_x = size_x;
-    this->size_y = size_y;
+    this->renderer = renderer;
+    this->window = window;
 }
 
-void game::init()
+void game::init_game_state(bool first_player)
 {
-    window = SDL_CreateWindow("Checkers", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, size_x, size_y, 0);
-    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    init_game_state_templ<true>(game_state);
 }
 
 void game::draw_board(bool first_player)
 {
     if (first_player)
     {
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    SDL_RenderClear(renderer);
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    }else{
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    SDL_RenderClear(renderer);
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    }
+    else
+    {
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        SDL_RenderClear(renderer);
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     }
 
     for (int i = 0; i < 8; i++)
@@ -37,8 +41,8 @@ void game::draw_board(bool first_player)
             }
         }
     }
-    
-    //right menu screen
+
+    // Right menu screen
     SDL_SetRenderDrawColor(renderer, 0, 102, 0, 255);
     SDL_Rect menu_rect = {640, 0, 360, 640};
     SDL_RenderFillRect(renderer, &menu_rect);
@@ -46,45 +50,63 @@ void game::draw_board(bool first_player)
     SDL_RenderPresent(renderer);
 }
 
-void game::events()
+void game::render_game_state()
 {
+    // TODO: Draw piecies based on game_state
+}
+
+void game::game_loop()
+{
+    // Game loop
     SDL_bool done = SDL_FALSE;
+    init_game_state(true);
     while (!done)
     {
-        SDL_Event event;
-        while (SDL_PollEvent(&event))
-        {
-            switch (event.type)
-            {
-            case SDL_QUIT:
-                done = SDL_TRUE;
-                break;
-
-            case SDL_MOUSEBUTTONDOWN:
-                if (event.button.button == SDL_BUTTON_LEFT)
-                {
-                    //relative position to window
-                    pos_xy[0] = event.button.x;
-                    pos_xy[1] = event.button.y;
-
-                    //rectangle position from 0 - 7
-                    if (pos_xy[0]>=640 || pos_xy[1]>=640)
-                        break;//if in menu screen
-                    
-                    rect_xy[0] = pos_xy[0] / 80;
-                    rect_xy[1] = pos_xy[1] / 80;
-
-                    std::cout << rect_xy[0] << rect_xy[1] << std::endl;
-                }
-            default:
-                break;
-            }
-        }
-
-        // SDL_GetMouseState();
+        // Gets input from mouse to rect_xy
+        events(done);
+        render_game_state();
     }
 
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
+}
+
+void game::events(SDL_bool &done)
+{
+
+    SDL_Event event;
+    while (SDL_PollEvent(&event))
+    {
+        switch (event.type)
+        {
+        case SDL_QUIT:
+            done = SDL_TRUE;
+            break;
+
+        case SDL_MOUSEBUTTONDOWN:
+            if (event.button.button == SDL_BUTTON_LEFT)
+            {
+                // Relative position to window
+                pos_xy[0] = event.button.x;
+                pos_xy[1] = event.button.y;
+
+                // Rectangle position from 0 - 7
+                if (pos_xy[0] >= 640 || pos_xy[1] >= 640)
+                    break; // if in menu screen
+
+                rect_xy[0] = pos_xy[0] / 80;
+                rect_xy[1] = pos_xy[1] / 80;
+
+                /*
+                If press is on piece, run method checkers::show_possible_moves,
+                if next press is on valid next move, then update game_state to current
+                */
+
+                // TODO: Add button to reset, quit, change first player
+            }
+        default:
+            break;
+        }
+    }
 }
