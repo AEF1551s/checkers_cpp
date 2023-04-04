2b95ddb382436f4b0eaaeced97793e166aeaa287
Krisjanis Ivbulis <ivbulisk@gmail.com>
2023-04-05 00:19:33 +0300

    fixed paths

--- src/texture_manager.cpp
+++ src/texture_manager.cpp
@@ -1,18 +1,24 @@
 #include "texture_manager.h"
+#include <iostream>
 
 std::vector<SDL_Texture *> texture_manager::list_textures_loaded;
 const std::string texture_manager::ASSETS_PATH = "assets/";
 
 SDL_Texture *texture_manager::load_texture(std::string filename, SDL_Renderer *renderer)
 {
+    /*
+    Base path: /home/roboros/Documents/checkers_cpp/build
+    Needed path /home/roboros/Documents/checkers_cpp   /assets
+    */
+    const std::string project_path = "/home/roboros/Documents/checkers_cpp/";
+
     // Setup the relative file_path to the images folder using the input filename.
-    std::string file_path = ASSETS_PATH + filename;
+    std::string file_path = project_path + ASSETS_PATH + filename;
 
     // Try to create a surface using the file_path.
     SDL_Surface *surface_temp = SDL_LoadBMP(file_path.c_str());
     if (surface_temp != nullptr)
     {
-
         // The surface was created successfully so attempt to create a texture with it.
         SDL_Texture *texture_output = SDL_CreateTextureFromSurface(renderer, surface_temp);
         // Free the surface because it's no longer needed.
@@ -22,13 +28,14 @@ SDL_Texture *texture_manager::load_texture(std::string filename, SDL_Renderer *r
         {
             // Enable transparency for the texture.
             SDL_SetTextureBlendMode(texture_output, SDL_BLENDMODE_BLEND);
-
+            
             // Add the texture to the list of loaded textures to keep track of it for clean-up purposes.
             list_textures_loaded.push_back(texture_output);
             return texture_output;
+
         }
     }
-
+    throw std::invalid_argument("could not load texture in texture_manager.cpp");
     return nullptr;
 }
 
