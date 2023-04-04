88ae51ec9efe5480d5eb6c25acdcb095240ad72e
Krisjanis Ivbulis <ivbulisk@gmail.com>
2023-04-04 16:47:06 +0300

    added load_texture method

--- include/game_board.h
+++ include/game_board.h
@@ -2,6 +2,7 @@
 #define GAME_BOARD_H
 
 #include <SDL2/SDL.h>
+#include "texture_manager.h"
 
 // Renders game pieces based on game_state
 // TODO: Show possible moves when pressed on game piece
@@ -10,11 +11,16 @@ class game_board
 private:
     SDL_Window *window;
     SDL_Renderer *renderer;
+
+    static SDL_Texture *texture_black, *texture_black_q,
+        *texture_white, *texture_white_q;
+
     static int game_state[8][8];
 
 public:
     game_board(SDL_Window &window, SDL_Renderer &renderer);
     void render_game_state();
+    void load_textures();
 };
 
 #endif // GAME_BOARD_H
--- src/game_board.cpp
+++ src/game_board.cpp
@@ -2,6 +2,9 @@
 
 int game_board::game_state[8][8];
 
+SDL_Texture *game_board::texture_black, *game_board::texture_black_q,
+    *game_board::texture_white, *game_board::texture_white_q;
+
 game_board::game_board(SDL_Window &window, SDL_Renderer &renderer)
 {
     this->window = &window;
@@ -11,4 +14,12 @@ game_board::game_board(SDL_Window &window, SDL_Renderer &renderer)
 void game_board::render_game_state()
 {
     // draw pieces on the board where the game state piece are
+}
+
+void game_board::load_textures()
+{
+    texture_black = texture_manager::load_texture("black_piece", renderer);
+    texture_black_q = texture_manager::load_texture("black_piece_q", renderer);
+    texture_white = texture_manager::load_texture("white_piece", renderer);
+    texture_white_q = texture_manager::load_texture("white_piece_q", renderer);
 }
