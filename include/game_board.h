d52b7dcf1ffe6f95ba590f8660127dc21f82014e
Krisjanis Ivbulis <ivbulisk@gmail.com>
2023-04-04 16:35:30 +0300

    updated class, added TODOs

--- include/game_board.h
+++ include/game_board.h
@@ -1,16 +1,19 @@
 #if !defined(GAME_BOARD_H)
 #define GAME_BOARD_H
 
-#include "templates.h"
+#include <SDL2/SDL.h>
 
+// Renders game pieces based on game_state
+// TODO: Show possible moves when pressed on game piece
 class game_board
 {
 private:
+    SDL_Window *window;
+    SDL_Renderer *renderer;
     static int game_state[8][8];
 
 public:
-    game_board(/* args */);
-    void init_game_state(bool first_player);
+    game_board(SDL_Window &window, SDL_Renderer &renderer);
     void render_game_state();
 };
 
--- src/game_board.cpp
+++ src/game_board.cpp
@@ -2,23 +2,13 @@
 
 int game_board::game_state[8][8];
 
-game_board::game_board(/* args */)
+game_board::game_board(SDL_Window &window, SDL_Renderer &renderer)
 {
+    this->window = &window;
+    this->renderer = &renderer;
 }
 
-void game_board::init_game_state(bool first_player)
+void game_board::render_game_state()
 {
-
-    if (first_player)
-    {
-        init_game_state_templ<true>(game_state);
-    }
-    else
-    {
-        init_game_state_templ<false>(game_state);
-    }
-}
-void game_board::render_game_state(){
-    //draw pieces on the board where the game state piece are
-
+    // draw pieces on the board where the game state piece are
 }
