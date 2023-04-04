d2ac9166daa334ee9347b3ed8eec2a6fecafab89
Krisjanis Ivbulis <ivbulisk@gmail.com>
2023-04-04 18:46:08 +0300

    fixed event loop

--- src/event_manager.cpp
+++ src/event_manager.cpp
@@ -1,31 +1,37 @@
 #include "event_manager.h"
-
+#include <iostream>
 event_manager::event_manager()
 {
 }
 
 void event_manager::events(SDL_bool &done)
 {
+    SDL_bool event_loop_done = SDL_FALSE;
     done = SDL_FALSE;
     SDL_Event event;
-    while (SDL_PollEvent(&event))
+    while ((SDL_PollEvent(&event) != 0 && !done) || !event_loop_done)
     {
+
         switch (event.type)
         {
         case SDL_QUIT:
             done = SDL_TRUE;
+            event_loop_done = SDL_TRUE;
             break;
 
         case SDL_MOUSEBUTTONDOWN:
             if (event.button.button == SDL_BUTTON_LEFT)
             {
                 handle_click(event.button.x, event.button.y);
-                //TODO: Show possible moves, if pressed on a piece.
-                //TODO: If pressed on possible move, update game_state
+
+                // TODO: Show possible moves, if pressed on a piece.
+                // TODO: If pressed on possible move, update game_state
             }
+            event_loop_done = SDL_TRUE;
             break;
             // TODO: Add button to reset, quit, change first player
         default:
+                event_loop_done = SDL_FALSE;
             break;
         }
     }
@@ -47,4 +53,5 @@ void event_manager::handle_click(int x, int y)
         rect_x = pos_x / 80;
         rect_y = pos_y / 80;
     }
+    std::cout << rect_x << " : " << rect_y << std::endl;
 }
