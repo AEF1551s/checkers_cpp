570233c936915c51033950a271c9d5a405887894
Krisjanis Ivbulis <ivbulisk@gmail.com>
2023-04-07 00:37:29 +0300

    added simple exception

--- include/game_state.h
+++ include/game_state.h
@@ -2,13 +2,12 @@
 #define GAME_STATE_H
 
 #include "templates.h"
+#include <stdexcept>
 
 namespace gamestate
 {
     extern int game_state[8][8];
-
     void init_game_state(bool first_player);
-
 }
 
 #endif // GAME_STATE_H
