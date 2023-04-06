1de5e6d688b366c13de26c060619202e150d0c59
Krisjanis Ivbulis <ivbulisk@gmail.com>
2023-04-07 00:34:00 +0300

    cleaned up code

--- src/game_state.cpp
+++ src/game_state.cpp
@@ -1,11 +1,19 @@
 #include "game_state.h"
-
+#include <iostream>
 int gamestate::game_state[8][8];
 
 void gamestate::init_game_state(bool first_player)
 {
-    if (first_player)
+    switch (first_player)
+    {
+    case true:
         init_game_state_templ<true>(gamestate::game_state);
-    else
+        break;
+    case false:
         init_game_state_templ<false>(gamestate::game_state);
+        break;
+    default:
+        throw std::invalid_argument("error in game_state.cpp");
+        break;
+    }
 }
