8cb5dd4a09a8f2a54887792108597a81d17fbdcd
Krisjanis Ivbulis <ivbulisk@gmail.com>
2023-04-09 13:04:53 +0300

    added possible_moves method and TODOs

--- include/event_manager.h
+++ include/event_manager.h
@@ -1,21 +1,26 @@
 #if !defined(event_manager_H)
 #define event_manager_H
 
+#include <vector>
 #include <SDL2/SDL.h>
 #include "game_state.h"
 
 class event_manager
 {
 private:
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+
     int rect_x, rect_y;
     int pos_x, pos_y;
 
 public:
-    event_manager();
+    event_manager(SDL_Window &window, SDL_Renderer &renderer);
     void events(SDL_bool &done);
     void events(SDL_bool &done, SDL_bool &reset);
     void events(SDL_bool &done, SDL_bool &reset, bool &first_player);
     void handle_click(int x, int y, SDL_bool &reset);
+    void possible_moves();
     bool check_if_clicked_piece();
     bool handle_reset_click(int x, int y, SDL_bool &reset, bool &first_player);
     bool check_button_press(int mouse_x, int mouse_y, int x, int y, int w, int h);
--- src/event_manager.cpp
+++ src/event_manager.cpp
@@ -1,8 +1,10 @@
 #include "event_manager.h"
 #include <iostream>
 
-event_manager::event_manager()
+event_manager::event_manager(SDL_Window &window, SDL_Renderer &renderer)
 {
+    this->window = &window;
+    this->renderer = &renderer;
 }
 
 void event_manager::events(SDL_bool &done)
@@ -47,14 +49,13 @@ void event_manager::events(SDL_bool &done, SDL_bool &reset, bool &first_player)
 
                         if (check_if_clicked_piece())
                         {
-                            std::cout << "pressed piece " << std::endl;
+                            possible_moves();
                         }
 
-                        // TODO: Show possible moves, if pressed on a piece.
                         // TODO: If pressed on possible move, update game_state
                         /*
             TODO: if pressed second time, check if allowed, and move piece,
-            change gamestate::game_state array. set event_loop_done = true
+            change state::game_state array. set event_loop_done = true
             otherwise event_loop_done = false, so there is no need to update the pieces on the table
             if nothing has moved.
             */
@@ -62,7 +63,7 @@ void event_manager::events(SDL_bool &done, SDL_bool &reset, bool &first_player)
                     }
                     else if (handle_reset_click(event.button.x, event.button.y, reset, first_player))
                     {
-                        gamestate::init_game_state(first_player);
+                        state::init_game_state(first_player);
                         event_loop_done = SDL_TRUE;
                     }
                 }
@@ -87,11 +88,9 @@ void event_manager::handle_click(int x, int y, SDL_bool &reset)
     else
     {
         // Rectangle position from 0 - 7
-        rect_x = x / 80;
-        rect_y = y / 80;
+        rect_x = y / 80;
+        rect_y = x / 80;
     }
-    std::cout << rect_x << " : " << rect_y << std::endl;
-    std::cout << x << " : " << y << std::endl;
 }
 
 bool event_manager::handle_reset_click(int x, int y, SDL_bool &reset, bool &first_player)
@@ -124,7 +123,91 @@ bool event_manager::check_if_clicked_piece()
     if (pos_x > 640)
         return false;
 
-    if (gamestate::game_state[rect_y][rect_x] != 0)
+    if (state::game_state[rect_x][rect_y] != 0)
+    {
+        // std::cout << "clicked piece " << std::endl;
         return true;
+    }
     return false;
+}
+
+void event_manager::possible_moves()
+{
+
+    using namespace state;
+
+    // Get the game state and the selected piece's position
+    int(*board)[8] = game_state;
+
+    int x = rect_y;
+    int y = rect_x;
+
+    // std::cout << board[y][x] << " " << x << " " << y << std::endl;
+
+    // Check if the selected piece is white or black
+    int player = board[y][x];
+    int opponent = (player == 1) ? 2 : 1;
+
+    // Calculate the possible moves for the selected piece
+    std::vector<std::pair<int, int>> moves;
+
+    if (player == 1)
+    {
+        // Check moves for white piece
+        if (y > 0)
+        {
+            if (x > 0 && board[y - 1][x - 1] == 0)
+            {
+                moves.emplace_back(x - 1, y - 1);
+            }
+            if (x < 7 && board[y - 1][x + 1] == 0)
+            {
+                moves.emplace_back(x + 1, y - 1);
+            }
+        }
+        if (y > 1 && x > 1 && board[y - 1][x - 1] == opponent && board[y - 2][x - 2] == 0)
+        {
+            moves.emplace_back(x - 2, y - 2);
+        }
+        if (y > 1 && x < 6 && board[y - 1][x + 1] == opponent && board[y - 2][x + 2] == 0)
+        {
+            moves.emplace_back(x + 2, y - 2);
+        }
+    }
+    else
+    {
+        // Check moves for black piece
+        if (y < 7)
+        {
+            if (x > 0 && board[y + 1][x - 1] == 0)
+            {
+                moves.emplace_back(x - 1, y + 1);
+            }
+            if (x < 7 && board[y + 1][x + 1] == 0)
+            {
+                moves.emplace_back(x + 1, y + 1);
+            }
+        }
+        if (y < 6 && x > 1 && board[y + 1][x - 1] == opponent && board[y + 2][x - 2] == 0)
+        {
+            moves.emplace_back(x - 2, y + 2);
+        }
+        if (y < 6 && x < 6 && board[y + 1][x + 1] == opponent && board[y + 2][x + 2] == 0)
+        {
+            moves.emplace_back(x + 2, y + 2);
+        }
+    }
+
+    // Color the possible moves green
+    // TODO: Moves this to game_board.
+    for (auto move : moves)
+    {
+        int mx = move.first;
+        int my = move.second;
+
+        SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255); // Green color
+
+        SDL_Rect rect = {mx * 80, my * 80, 80, 80};
+        SDL_RenderFillRect(renderer, &rect);
+    }
 }
