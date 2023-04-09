4483d1953419c8860ed19ce41f2933c1fd45f44b
Krisjanis Ivbulis <ivbulisk@gmail.com>
2023-04-09 17:13:39 +0300

    changed arguments

--- include/game_state.h
+++ include/game_state.h
@@ -9,7 +9,7 @@ namespace state
     extern int game_state[8][8];
 
     void init_game_state(bool first_player);
-    bool update_game_state(int next_x, int next_y, int prev_x, int prev_y, bool player_move);
+    void update_game_state(int next_x, int next_y, int prev_x, int prev_y, bool player_move);
 }
 
 #endif // GAME_STATE_H
