5ac6a36d2e8348a0a39a7a72f601efb94566ae3c
Krisjanis Ivbulis <ivbulisk@gmail.com>
2023-04-09 13:06:00 +0300

    updated namestate name

--- include/game_state.h
+++ include/game_state.h
@@ -4,7 +4,7 @@
 #include "templates.h"
 #include <stdexcept>
 
-namespace gamestate
+namespace state
 {
     extern int game_state[8][8];
     void init_game_state(bool first_player);
--- src/game_state.cpp
+++ src/game_state.cpp
@@ -1,16 +1,16 @@
 #include "game_state.h"
 #include <iostream>
-int gamestate::game_state[8][8];
+int state::game_state[8][8];
 
-void gamestate::init_game_state(bool first_player)
+void state::init_game_state(bool first_player)
 {
     switch (first_player)
     {
     case true:
-        init_game_state_templ<true>(gamestate::game_state);
+        init_game_state_templ<true>(state::game_state);
         break;
     case false:
-        init_game_state_templ<false>(gamestate::game_state);
+        init_game_state_templ<false>(state::game_state);
         break;
     default:
         throw std::invalid_argument("error in game_state.cpp");
