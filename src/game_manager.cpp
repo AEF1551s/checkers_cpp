ab815df79dbfb0afbb8996c6c5605c2f1a5e6b94
Krisjanis Ivbulis <ivbulisk@gmail.com>
2023-04-11 10:46:03 +0300

    added delay, until fix.

--- src/game_manager.cpp
+++ src/game_manager.cpp
@@ -41,6 +41,8 @@ void game_manager::game_loop()
             game_board.render_game_state();
             event_manager.events(done, reset, first_player);
             SDL_RenderPresent(renderer);
+            
+            SDL_Delay(100);
         }
     }
 
